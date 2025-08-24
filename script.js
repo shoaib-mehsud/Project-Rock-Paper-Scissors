beaf9d36dcce7db9d2d60d1cd922cb1694a6649f
shoaib-mehsud <226695319+shoaib-mehsud@users.noreply.github.com>
2025-08-24 22:14:11 +0500

    feat: implement single round gameplay

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // it will randomly return one of these "Rock,Paper or Scissors"
 function getComputerChoice() {
     let compValue = (Math.floor(Math.random()*3)+1);
-
+    
     if(compValue === 1){
        return "scissors";
     }
@@ -12,12 +12,62 @@ function getComputerChoice() {
         return "rock"
     }
 
-    }
-console.log(getComputerChoice());
+    }  
+
 
 function getHumanChoice() {
-    let humnValue = prompt("Enter your choice? Rock paper or Scissors");
-    return humnValue.toLocaleLowerCase();
+    let humnValue = prompt("Enter your choice? Rock paper or Scissors?" );
+    let insensitive = humnValue.toLowerCase();
+    return insensitive;
 }
+  
+
+
+let compScore = 0;
+let humanScore = 0;
+
+function playRound(humanChoice,compChoice) {
+    
+    if(humanChoice == "rock" && compChoice == "scissors"){
+       console.log("You Win! Rock beat Scissors");
+       humanScore = humanScore + 1;
+    }
+
+    else if(humanChoice == "paper" && compChoice == "scissors"){
+       console.log("You Lose! Scissors beats Paper");
+        compScore++;
+    }
+
+    else if(humanChoice == "paper" && compChoice == "rock"){
+        console.log("You Win! Paper beat Rock");
+        humanScore++;    
+    }
+
+    else if (humanChoice == "rock" && compChoice == "paper") {
+        console.log("You Lose! Paper beats Rock");
+        compScore++;
+    }
+
+    else if (humanChoice == "scissors" && compChoice == "rock") {
+        console.log("You Lose! Rock beats Scissors");
+        compScore++;
+    }
+
+    else if (humanChoice == "scissors" && compChoice == "paper") {
+        console.log("You Win! Scissors beats Paper");
+        humanScore++;
+    }
+
+    else if(humanChoice == "paper" && compChoice == "paper" || humanChoice == "rock" && compChoice == "rock" || humanChoice == "scissors" && compChoice == "scissors"){
+        console.log("Tie!")
+    }
+}
+   let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+       console.log(`Computers value: ${computerSelection}`);
+
+
+    playRound(humanSelection,computerSelection);
+
 
 
