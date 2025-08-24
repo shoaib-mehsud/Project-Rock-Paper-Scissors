321ec2263b9d8a762f41214d9116b3e47cace241
shoaib-mehsud <226695319+shoaib-mehsud@users.noreply.github.com>
2025-08-24 22:39:59 +0500

    feat: implement five-round console-based gameplay
    - Added logic for playing 5 rounds of Rock-Paper-Scissors
    - Tracks score of both human and computer
    - Declares the final winner at the end
    - Currently console-based implementation
    - Will be extended later to GUI version

--- script.js
+++ script.js
@@ -29,45 +29,77 @@ let humanScore = 0;
 function playRound(humanChoice,compChoice) {
     
     if(humanChoice == "rock" && compChoice == "scissors"){
-       console.log("You Win! Rock beat Scissors");
+       alert("You Win! Rock beat Scissors");
+              console.log("You Win! Rock beat Scissors");
+
        humanScore = humanScore + 1;
     }
 
     else if(humanChoice == "paper" && compChoice == "scissors"){
-       console.log("You Lose! Scissors beats Paper");
+       alert("You Lose! Scissors beats Paper");
+        console.log("You Lose! Scissors beats Paper");
         compScore++;
     }
 
     else if(humanChoice == "paper" && compChoice == "rock"){
+        alert("You Win! Paper beat Rock");
         console.log("You Win! Paper beat Rock");
         humanScore++;    
     }
 
     else if (humanChoice == "rock" && compChoice == "paper") {
+        alert("You Lose! Paper beats Rock");
         console.log("You Lose! Paper beats Rock");
         compScore++;
     }
 
     else if (humanChoice == "scissors" && compChoice == "rock") {
+        alert("You Lose! Rock beats Scissors");
         console.log("You Lose! Rock beats Scissors");
         compScore++;
     }
 
     else if (humanChoice == "scissors" && compChoice == "paper") {
+        alert("You Win! Scissors beats Paper");
         console.log("You Win! Scissors beats Paper");
+
         humanScore++;
     }
 
     else if(humanChoice == "paper" && compChoice == "paper" || humanChoice == "rock" && compChoice == "rock" || humanChoice == "scissors" && compChoice == "scissors"){
-        console.log("Tie!")
+        alert("Tie!");
+        console.log("Tie!");
     }
 }
-   let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-       console.log(`Computers value: ${computerSelection}`);
+   
 
 
-    playRound(humanSelection,computerSelection);
+   
 
 
+    function playGame() {
+        for(let i = 1; i<=5; i++){
+            alert(`Round:${i} `)
+            let humanSelection = getHumanChoice();
+            console.log(`You Entered: ${humanSelection}`);
+            let computerSelection = getComputerChoice();
+             console.log(`Computers value: ${computerSelection}`);
+             playRound(humanSelection,computerSelection);
+        }
+    }
+    playGame();
 
+    if (humanScore > compScore){
+        alert(`You Win! your score: ${humanScore} , computer score: ${compScore}` );
+         console.log(`You Win! your score: ${humanScore} , computer score: ${compScore}` );
+    }
+    else if (humanScore < compScore){
+        alert(`You Lose! your score: ${humanScore} , computer score: ${compScore}` );
+        console.log(`You Lose! your score: ${humanScore} , computer score: ${compScore}` );
+
+    }
+     else{
+        alert(`Tie! your score: ${humanScore} , computer score: ${compScore}` );
+        console.log(`Tie! your score: ${humanScore} , computer score: ${compScore}` );
+
+    }
