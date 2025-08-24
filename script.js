4170903bdeb27c3509826ccc169d95a7f837dbad
shoaib-mehsud <226695319+shoaib-mehsud@users.noreply.github.com>
2025-08-24 15:37:22 +0500

    feat: Add computer random choice generator
    - Computer will randomly generate any one value among Rock Paper Scissor

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+// it will randomly return one of these "Rock,Paper or Scissors"
+function getComputerChoice() {
+    let compValue = (Math.floor(Math.random()*3)+1);
+
+    if(compValue === 1){
+       return "scissors";
+    }
+    else if(compValue === 2){
+        return "paper"
+    }
+    else{
+        return "rock"
+    }
+
+    }
+console.log(getComputerChoice());
+
